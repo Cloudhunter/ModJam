dbceacba389a4c64c00f328aff4a165d95089c69
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2013-12-14 14:58:38 +1100

    Primitive serializer in 15 minutes? Hell yeah.

--- modsrc/uk/co/cloudhunter/rpgthing/database/Database.java
+++ modsrc/uk/co/cloudhunter/rpgthing/database/Database.java
@@ -11,6 +11,22 @@ import java.util.HashMap;
 
 public class Database {
 
+	private static final Class<?>[] packRules = { int.class, Integer.class, boolean.class, Boolean.class, double.class,
+			Double.class, float.class, Float.class, String.class };
+
+	private static int getPackRule(Class<?> clazz) {
+		for (int i = 0; i < packRules.length; i++)
+			if (clazz.equals(packRules[i]))
+				return i;
+		return -1;
+	}
+
+	private static Class<?> getUnpackRule(int k) {
+		if (k >= 0 && k < packRules.length)
+			return packRules[k];
+		return null;
+	}
+
 	public static class Table {
 		private HashMap<Integer, Class<?>> struct;
 		private HashMap<Integer, String> labels;
@@ -65,7 +81,40 @@ public class Database {
 				if (row != null) {
 					out.writeInt(i);
 					for (int j = 0; j < struct.size(); j++) {
-
+						Object value = row.get(j);
+						if (value == null)
+							out.writeInt(-1);
+						else {
+							int typeof = getPackRule(value.getClass());
+							if (typeof == -1)
+								throw new UnsupportedOperationException("Can't pack non primitive type, fail.");
+							switch (typeof) {
+								case 0:
+								case 1:
+									out.writeInt((Integer) value);
+									break;
+								case 2:
+								case 3:
+									out.writeByte((Boolean) value ? 1 : 0);
+									break;
+								case 4:
+								case 5:
+									out.writeDouble((Double) value);
+									break;
+								case 6:
+								case 7:
+									out.writeFloat((Float) value);
+									break;
+								case 8:
+									String s = (String) value;
+									out.writeInt(s.length());
+									for (char c : s.toCharArray())
+										out.writeChar(c);
+									break;
+								default:
+									throw new UnsupportedOperationException("Strange primitive type, fail.");
+							}
+						}
 					}
 				}
 			}
@@ -73,6 +122,36 @@ public class Database {
 
 		public void load(InputStream stream) throws IOException {
 			DataInputStream in = new DataInputStream(stream);
+			int num_rows = in.readInt();
+			auto_inc = in.readInt();
+			for (int i = 0; i < num_rows; i++) {
+				HashMap<Integer, Object> row = new HashMap<Integer, Object>();
+				int row_id = in.readInt();
+				for (int j = 0; j < struct.size(); j++) {
+					int typeof = in.readInt();
+					if (typeof == -1)
+						row.put(j, null);
+					else {
+						Class<?> of = getUnpackRule(typeof);
+						if (of.equals(int.class) || of.equals(Integer.class))
+							row.put(j, in.readInt());
+						else if (of.equals(boolean.class) || of.equals(Boolean.class))
+							row.put(j, (in.readByte() != 0));
+						else if (of.equals(double.class) || of.equals(Double.class))
+							row.put(j, in.readDouble());
+						else if (of.equals(float.class) || of.equals(Float.class))
+							row.put(j, in.readFloat());
+						else if (of.equals(String.class)) {
+							int length = in.readInt();
+							StringBuilder result = new StringBuilder();
+							for (int k = 0; k < length; k++)
+								result.append(in.readChar());
+							row.put(j, result.toString());
+						} else
+							throw new UnsupportedOperationException("Strange primitive type, fail.");
+					}
+				}
+			}
 		}
 	}
 
