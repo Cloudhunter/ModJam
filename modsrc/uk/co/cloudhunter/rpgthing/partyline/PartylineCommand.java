ab16eb23a3077d83c515f625cf5567026e827e19
Cloudhunter <cloudhunter@btinternet.com>
2013-12-14 22:15:05 +0000

    Much commit. Very auto. Wow.

--- modsrc/uk/co/cloudhunter/rpgthing/partyline/PartylineCommand.java
+++ modsrc/uk/co/cloudhunter/rpgthing/partyline/PartylineCommand.java
@@ -2,6 +2,7 @@ package uk.co.cloudhunter.rpgthing.partyline;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 
 public class PartylineCommand extends CommandBase {
 
@@ -12,14 +13,19 @@ public class PartylineCommand extends CommandBase {
 
 	@Override
 	public String getCommandUsage(ICommandSender icommandsender) {
-		// TODO Auto-generated method stub
-		return null;
+		return "commands.party.usage";
 	}
 
 	@Override
-	public void processCommand(ICommandSender icommandsender, String[] astring) {
-		// TODO Auto-generated method stub
-
+	public void processCommand(ICommandSender icommandsender, String[] args) {
+        if (args.length == 0)
+        {
+            throw new WrongUsageException("commands.party.usage");
+        }
+        else if ("help".equals(args[0]))
+        {
+            throw new WrongUsageException("commands.forge.usage");
+        }
 	}
 
 }
