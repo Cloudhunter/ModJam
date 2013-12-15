49b22bb741a5f651ebbdc6effd03790951c1df3e
Cloudhunter <cloudhunter@btinternet.com>
2013-12-15 20:45:04 +0000

    Much commit. Very auto. Wow.

---
+++ modsrc/uk/co/cloudhunter/rpgthing/gui/trading/ContainerTrading.java
@@ -0,0 +1,16 @@
+package uk.co.cloudhunter.rpgthing.gui.trading;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+public class ContainerTrading extends Container
+{
+
+	@Override
+	public boolean canInteractWith(EntityPlayer entityplayer)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ modsrc/uk/co/cloudhunter/rpgthing/gui/trading/GuiTrading.java
@@ -0,0 +1,20 @@
+package uk.co.cloudhunter.rpgthing.gui.trading;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+public class GuiTrading extends GuiContainer
+{
+
+	public GuiTrading(Container par1Container) {
+		super(par1Container);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
