e25fe1dd29eaca49f853c5ef56b146b39047eb38
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2013-12-15 12:39:34 +1100

    Adding player network data handler

---
+++ modsrc/uk/co/cloudhunter/rpgthing/core/PlayerDataNetworkHelper.java
@@ -0,0 +1,21 @@
+package uk.co.cloudhunter.rpgthing.core;
+
+import uk.co.cloudhunter.rpgthing.network.ModPacket;
+import uk.co.cloudhunter.rpgthing.network.StandardModPacket;
+import cpw.mods.fml.common.network.Player;
+
+public class PlayerDataNetworkHelper {
+
+	public void accept(ModPacket packet, Player player, boolean isServer) {
+		StandardModPacket sp = (StandardModPacket) packet;
+		if (!isServer) {
+			String req = (String) sp.getValue("payload");
+			if (req.equals("player-data")) {
+				String target = (String) sp.getValue("player-name");
+				uk.co.cloudhunter.rpgthing.core.Player p = uk.co.cloudhunter.rpgthing.core.Player.getPlayer(target);
+				p.readFromPacket(sp, "player-data");
+			}
+		}
+	}
+
+}
